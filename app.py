d04cf73b0b9771d8e3a30533063ae23916907b8c
jump <jpaulinophb@gmail.com>
2019-12-08 04:22:49 -0300

    implementado o upload

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
+import os
 
+# Estrutura básica do flask
 app = Flask(__name__,template_folder='templates',static_folder='static')
-
+UPLOAD_FOLDER = os.path.join(os.getcwd(),'upload')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/upload',methods=['POST'])
+def upload():
+    file = request.files['teste']
+    savePath = os.path.join(UPLOAD_FOLDER,secure_filename(file.filename))
+    file.save(savePath)
+    return 'ok'
+
+#-------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
+
---
+++ static/js/index.js
@@ -0,0 +1,3 @@
+function fazUpload() {
+    document.enviarArquivo.submit();
+}
